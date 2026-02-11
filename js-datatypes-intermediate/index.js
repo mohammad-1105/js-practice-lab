8289fb94150fd710eb1928aff9003b32510282f7
Mohammand Aman <mohammadamaan1105@gmail.com>
2026-02-11 22:21:19 +0545

    chore: deleted unused file index.js

--- js-datatypes-intermediate/index.js
+++
@@ -1,9 +0,0 @@
-const players = [
-  { name: "Dhoni", role: "wk", price: 1200 },
-  { name: "Virat", role: "wk", price: 1300 },
-  { name: "Bumra", role: "wk", price: 1400 },
-  { name: "Rohit", role: "wk", price: 1500 },
-];
-
-const totalPrice = players.reduce((sum, p) => sum + p.price, 0);
-console.log(totalPrice);
